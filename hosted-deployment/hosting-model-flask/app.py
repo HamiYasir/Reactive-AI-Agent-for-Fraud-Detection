4284e34a80365fe5fb10d27e5f577f26600a6286
Hami Yasir <hamiyasir06@gmail.com>
2026-06-20 19:37:07 +0530

    commit #4. Deployment adjustments.

--- hosted-deployment/hosting-model-flask/app.py
+++ hosted-deployment/hosting-model-flask/app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -47,4 +47,7 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
